32f065f5290bd8f6ef1debdb308f329a9b047802
Raúl Aguilar <ag21raul@gmail.com>
2022-05-29 13:44:22 +0200

    Update skills

--- src/components/Home/Skills.vue
+++ src/components/Home/Skills.vue
@@ -7,13 +7,14 @@
     <div id="skills-svg">
       <img src="../../assets/images/logo/html5.svg" title="HTML5"/>
       <img src="../../assets/images/logo/css3.svg" title="CSS3"/>
-      <img src="../../assets/images/logo/sass.svg" title="SASS"/>
-      <img src="../../assets/images/logo/javascript.svg" title="Javascript"/>
+      <img src="../../assets/images/logo/sass.svg" title="SASS/SCSS"/>
+      <img src="../../assets/images/logo/javascript.svg" title="JavaScript"/>
       <img src="../../assets/images/logo/nodejs.svg" title="NodeJS"/>
-      <img src="../../assets/images/logo/jquery.svg" title="JQuery"/>
-      <img src="../../assets/images/logo/vue.svg" title="VueJS"/>
-      <img src="../../assets/images/logo/git.svg" title="Git"/>
+      <img src="../../assets/images/logo/vue.svg" title="Vue 3"/>
+      <img src="../../assets/images/logo/java.svg" title="Java"/>
       <img src="../../assets/images/logo/mongodb.svg" title="MongoDB"/>
+      <img src="../../assets/images/logo/mysql.svg" title="MySQL"/>
+      <img src="../../assets/images/logo/git.svg" title="Git"/>
     </div>
   </div>
 </template>
